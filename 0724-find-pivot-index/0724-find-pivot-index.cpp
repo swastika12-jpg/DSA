44a12320072dbfa50188c1086d7dbaa0bcf3f634
Swastika dubey <swastikadubey4@gmail.com>
2026-07-22 00:06:58 +0530

    Time: 0 ms (100%), Space: 35.7 MB (56.2%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+        }
+        
+
+        int left = 0;
+        for(int i = 0; i < nums.size(); i++){
+            //left += nums[i-1];
+            int right = sum - left - nums[i];
+            if(left == right) return i;
+            left += nums[i];
+        }
+        return -1;
+    }
+};
