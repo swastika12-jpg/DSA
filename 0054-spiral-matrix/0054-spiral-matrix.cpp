1374397fb6bc88248a8555379db61aeec3cf7225
Swastika dubey <swastikadubey4@gmail.com>
2026-08-03 23:15:53 +0530

    Time: 0 ms (100%), Space: 9.4 MB (18.15%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,49 +1,45 @@
 class Solution {
 public:
-   vector<int> spiralOrder(vector<vector<int>>& matrix) {
-    vector<int> res;
-    if (matrix.empty()) return res;
-    
-    int top = 0, bottom = matrix.size() - 1;
-    int left = 0, right = matrix[0].size() - 1;
-    
-    while (top <= bottom && left <= right) {
-        
-        // left to right (for loop)
-        for (int i = left; i <= right; i++) {
-            res.push_back(matrix[top][i]);
-        }
-        top++;
-        
-        // top to bottom (while loop)
-        int i = top;
-        while (i <= bottom) {
-            res.push_back(matrix[i][right]);
-            i++;
-        }
-        right--;
-        
-        // right to left (for loop)
-        if (top <= bottom) {
-            for (int i = right; i >= left; i--) {
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int>res;
+        if(matrix.empty())   return res;
+
+        int left =0; int top= 0;
+        int n=matrix.size();
+        int m = matrix[0].size();
+        int bottom = n-1;
+        int right=m-1;
+
+        while(left <= right && top <= bottom){
+            //left to right
+            for(int i=left; i<= right;i++){   
+                res.push_back( matrix[top][i]);
+            }
+                top++;
+            
+            //top to bottom
+            for(int i=top; i <= bottom;i++ ){
+                res.push_back(matrix[i][right]);
+            }
+                right--;
+            
+            //right to left
+            if(top<=bottom){
+            for(int i =right; i >= left;i--){
                 res.push_back(matrix[bottom][i]);
             }
-            bottom--;
-        }
-        
-        // bottom to top (while loop)
-        if (left <= right) {
-            int i = bottom;
-            while (i >= top) {
-                res.push_back(matrix[i][left]);
-                i--;
+                bottom--;
             }
-            left++;
+            
+            //bottom to top
+            if(left <= right){
+            for(int i =bottom; i >= top; i--){
+                res.push_back( matrix[i][left]);
+            }
+                left++;
+            }
         }
+
+      return res;  
     }
-    
-    return res;
-}
-        
-    
 };
